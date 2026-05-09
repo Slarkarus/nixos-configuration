d8bc2844974bd5925b397a244d553915fcd34eb8
Mayer Alexey <slarkarus@gmail.com>
2026-05-10 01:49:34 +0300

    Add steam

--- configuration.nix
+++ configuration.nix
@@ -125,6 +125,13 @@
         };
     };
 
+    programs.steam = {
+        enable = true;
+        remotePlay.openFirewall = true;
+        dedicatedServer.openFirewall = true;
+        localNetworkGameTransfers.openFirewall = true;
+    };
+
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
     programs.ssh.startAgent = true;
